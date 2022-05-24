1436d7fd7ed1321d6cdbcc6b5ea467de7e911f69
Cristian Chaparro <cristian.chaparro@correounivalle.edu.co>
2022-05-24 18:18:10 -0500

    Ingresar con API

--- app/src/main/java/com/example/examenpractico/MainActivity.java
+++ app/src/main/java/com/example/examenpractico/MainActivity.java
@@ -1,31 +1,51 @@
 package com.example.examenpractico;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
     EditText ed1, ed2;
-    Button btnIngresar;
+    String[] usuarios = new String[3];
+    String[] password = new String[3];
+    String[] nombres = new String[3];
+    int RC_SIGN_IN = 1;
+    String TAG = "GoogleSignIn";
+
+
     //Variable para gestionar FirebaseAuth
     private FirebaseAuth mAuth;
     //Agregar cliente de inicio de sesión de Google
@@ -37,8 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ed1 = findViewById(R.id.ed1);
         ed2 = findViewById(R.id.ed2);
-        btnIngresar = findViewById(R.id.btnIngresar);
-        btnIngresar.setOnClickListener(this);
 
         // Configurar Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -46,9 +64,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .requestEmail()
                 .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    public void signIn(View m) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    public void Ingresar(View l){
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if(task.isSuccessful()){
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    Log.w(TAG, "Google sign in failed", e);
+                }
+            }else{
+                Log.d(TAG, "Error, login no exitoso:" + task.getException().toString());
+                Toast.makeText(this, "Ocurrio un error. "+task.getException().toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            Intent homeActivity = new Intent(MainActivity.this,Home.class);
+                            startActivity(homeActivity);
+                            MainActivity.this.finish();
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
+    }
+    //si ya esta loguado no tiene porque volver a loguearse se envia directamente a home
+    @Override
+    protected void onStart() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null){
+            Intent homeActivity = new Intent(MainActivity.this,Home.class);
+            startActivity(homeActivity);
+            MainActivity.this.finish();
+        }
+        super.onStart();
+    }
+
+
+
+
+    public void Ingresar(){
         Intent ir = new Intent(this,Home.class);
         ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TASK | ir.FLAG_ACTIVITY_CLEAR_TOP);
         if(ed1.getText().toString().matches("") || ed2.getText().toString().matches("")){
@@ -57,10 +136,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             AlertDialog dialog = builder.create();
             dialog.show();
         }else {
-            Bundle datos = new Bundle();
-            datos.putString("name",ed1.getText().toString());
-            ir.putExtras(datos);
-            startActivity(ir);
+            String vEmail = ed1.getText().toString();
+            String vPassword = ed2.getText().toString();
+            for(int i=0; i<usuarios.length;i++){
+                if(usuarios[i].equals(vEmail) && password[i].equals(vPassword)){
+                    Bundle datos = new Bundle();
+                    datos.putString("name",ed1.getText().toString());
+                    ir.putExtras(datos);
+                    startActivity(ir);
+                }
+            }
+
         }
     }
 
@@ -74,13 +160,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray jsonArray = response.getJSONArray("usuarios");
-                            String usuarios = "";
                             for (int i = 0; i < response.getInt("count"); i++) {
                                 JSONObject jsonObject2 = jsonArray.getJSONObject(i);
-                                usuarios += jsonObject2.getString("email") +"\n";
-                                usuarios += jsonObject2.getString("contraseña") +"\n";
-                                usuarios += jsonObject2.getString("nombre") +"\n";
+                                usuarios[i] = jsonObject2.getString("email");
+                                password[i] = jsonObject2.getString("contraseña");
+                                nombres[i] = jsonObject2.getString("nombre");
                             }
+                            Ingresar();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -99,10 +185,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         queue.add(req);
     }
 
-
-    @Override
-    public void onClick(View view) {
-        dataVolley(view);
-        Ingresar(view);
-    }
 }
